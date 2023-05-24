64c57279ee833288584d70cbe14bfe97a3b31c2d
Willames <willamessilva1999@gmail.com>
2023-05-24 09:50:26 -0300

    feat: query params is now usable

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,8 +2,17 @@ const users = require('../mocks/users')
 
 
 module.exports = {
-    listUsers(response){
+    listUsers(request, response){
+        const {order} = request.query
+
+        const userSorteds = users.sort((a, b)=>{
+            if(order === 'dsc'){
+                return a.id < b.id ? 1 : -1;
+            }
+            return a.id > b.id ? 1 : -1;
+        })
+
         response.writeHead(200, {'content-type': 'application/json'});
-        response.end(JSON.stringify(users));
+        response.end(JSON.stringify(userSorteds));
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const http = require('http');
+const {URL} = require('url');
 const routes = require('./routes.js');
+const { parse } = require('path');
 
 const server = http.createServer((request, response)=>{
 
+    const parsedUrl = new URL(`http://localhost:3000${request.url}`)
+
+    console.log(request.url)
+
     const route = routes.find((objectRoute)=>{
-        return objectRoute.endpoint === request.url && objectRoute.method === request.method
+        return objectRoute.endpoint === parsedUrl.pathname && objectRoute.method === request.method
     })
 
     if(route) {
-        route.handler(response)
+        request.query = Object.fromEntries(parsedUrl.searchParams)
+        route.handler(request, response)
     }else{
         response.writeHead(404, {'content-type': 'text/html'});
         response.end('<h1>404</h1>');
